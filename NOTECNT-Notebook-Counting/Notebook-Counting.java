ae9c37c4c80d0c00de3c676386834b7e58d4abc9
Trivikram Varma <232066309+trivikram17@users.noreply.github.com>
2026-01-29 11:30:15 +0530

    Solved: Notebook Counting (NOTECNT)

---
+++ NOTECNT-Notebook-Counting/Notebook-Counting.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner sc = new Scanner(System.in); 
+	int A = sc.nextInt(); 
+	int B = sc.nextInt(); 
+	System.out.println((A*B)*100);
+
+	}
+}
