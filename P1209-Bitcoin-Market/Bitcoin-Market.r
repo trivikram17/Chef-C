e65ee35d280e032f2bde7d315b3cad14750124ee
Trivikram Varma <232066309+trivikram17@users.noreply.github.com>
2026-01-29 11:20:04 +0530

    Solved: Bitcoin Market (P1209)

---
+++ P1209-Bitcoin-Market/Bitcoin-Market.r
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=4){
+		    System.out.println("Yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
