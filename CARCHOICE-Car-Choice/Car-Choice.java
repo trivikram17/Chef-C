12e88a8c908f6f78a47e73d277bc026db690c5eb
Trivikram Varma <232066309+trivikram17@users.noreply.github.com>
2026-02-09 14:28:54 +0530

    Solved: Car Choice (CARCHOICE)

--- CARCHOICE-Car-Choice/Car-Choice.java
+++ CARCHOICE-Car-Choice/Car-Choice.java
@@ -1,31 +1,26 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc= new Scanner(System.in);
-		int t=sc.nextInt();
-		while(t-- >0){
-		    int x1=sc.nextInt();
-		    int x2=sc.nextInt();
-		    int y1=sc.nextInt();
-		    int y2=sc.nextInt();
-		    
-		    if(y1/x1 > y2/x2){
-		        System.out.println(1);
-		    }else if (y1/x1 < y2/x2){
-		        System.out.println(-1);
-		        
-		    }else{
-		        System.out.println(0);
-		    }
-		    
-		}
-		
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
 
-	}
+        while (t-- > 0) {
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int y2 = sc.nextInt();
+
+            int cost1 = y1 * x2;
+            int cost2 = y2 * x1;
+
+            if (cost1 < cost2) {
+                System.out.println(-1);
+            } else if (cost1 > cost2) {
+                System.out.println(1);  
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
 }
